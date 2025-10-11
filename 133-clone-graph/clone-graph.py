1c84604f87c9ad78b793457ec03cd8a78cca4111
Md Rabib Noor <rabibnoor@gmail.com>
2025-10-11 15:01:23 -0500

    Time: 96 ms (5.76%) | Memory: 12.9 MB (24.13%) - LeetSync

---
+++ 133-clone-graph/clone-graph.py
@@ -0,0 +1,42 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution(object):
+    all_nodes = []
+    gen_nodes = []
+    def traverse(self, node):
+        if node:
+            self.all_nodes.append(node)
+            copy = Node()
+            copy.val = node.val
+            self.gen_nodes.append(copy)
+            print(node.val)
+            for n in node.neighbors:
+                if n:
+                    if n not in self.all_nodes:
+                        newcopy = self.traverse(n)
+                        copy.neighbors.append(newcopy)
+                    else:
+                        idx = self.all_nodes.index(n)
+                        copy.neighbors.append(self.gen_nodes[idx])
+
+            return copy
+
+
+    def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
+        self.all_nodes = []
+        self.gen_nodes = []
+        copy = self.traverse(node)
+        return copy
+
+
+        
