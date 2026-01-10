4f8ffbe8a70f49f338d44437bfeedb1d716172da
Md Rabib Noor <rabibnoor@gmail.com>
2026-01-09 18:34:42 -0600

    Time: 4 ms (20.62%) | Memory: 15.4 MB (6.02%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -5,55 +5,25 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
-
-    def qpush(self,node, myQ):
-        myQ.append(node)
- 
-    def qpop(self,myQ):
-       node = myQ.pop(0)
-       return node
-    
-    def bfs(self,myQ, ret):
-        if not myQ:
-            return ret
-        item = self.qpop(myQ)
-        node = item[0]
-        level = item[1]
-        #print("now",(node.val), (level), len(myQ))
-
-        
-        #print(ret)
-        #print(len(ret))
-        if len(ret) < level+1:
-            ret.append([node.val])
-        else:
-            ret[level].append(node.val)
-
-        if node.left:
-            self.qpush((node.left, level+1), myQ)
-        
-        if node.right:
-            self.qpush((node.right, level+1), myQ)
-        
-        #print("q",[v[0].val for v in myQ])
-        #print("ret", ret)
-        self.bfs(myQ, ret)
-        return ret
-
-        
-
     def levelOrder(self, root):
         """
         :type root: Optional[TreeNode]
         :rtype: List[List[int]]
         """
-        if not root:
-            return []
-        myQ = []
-        self.qpush((root, 0), myQ)
-        
+        level = []
 
-        return self.bfs(myQ, [])
+        def dfs(root, deep):
+            if not root:
+                return
+            if len(level) < deep + 1:
+                level.append([])
+            level[deep].append(root.val)
+            
+            dfs(root.left, deep+1)
+            dfs(root.right, deep+1)
 
-      
-        
+        dfs(root, 0)
+
+        return level
+            
+
