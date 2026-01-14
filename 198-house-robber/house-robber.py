155bfc1a4c45f4285ddc8c636e162b2aaeefce55
Md Rabib Noor <rabibnoor@gmail.com>
2026-01-13 22:54:24 -0600

    Time: 0 ms (100.00%) | Memory: 12.6 MB (2.09%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -5,14 +5,14 @@ class Solution(object):
         :rtype: int
         """
         robbed = {}
-        def torob(i, t):
+        def torob(i):
             if i >= len(nums):
                 return 0
             if i in robbed:
                 return robbed[i]
-            roby = torob(i+2, t+nums[i])
+            roby = torob(i+2)
             
-            robn = torob(i+1, t) 
+            robn = torob(i+1) 
             robbed[i] = max(nums[i]+roby, robn)
             return robbed[i] 
-        return torob(0, 0)
+        return torob(0)
